7ce9ee7ca98bdbb3245b845a1421357607c2c99b
Alexander <altz7@hotmail.com>
2025-01-23 13:05:15 +0100

    remove and update files

--- demo_test.py
+++ demo_test.py
@@ -6,5 +6,5 @@ enbios_mod = SoftLink(r'C:\Users\altz7\PycharmProjects\ENBIOS4TIMES_\testing\dat
                       )
 
 
-enbios_mod.preprocess(national=True)
+enbios_mod.preprocess()
 enbios_mod.data_for_ENBIOS(smaller_vers=False, path_save=r'test.json')
